d4fa963ae035214684b2440667ca498806478396
ankush <ankushbihal8@gmail.com>
2021-02-16 19:47:14 +0100

    Commit 3
    este commit 3 afecta a los mismos archivos que el commit 2

--- resources/views/layouts/4Footer.blade.php
+++ resources/views/layouts/4Footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer mt-auto py-3 bg-light">
   <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+    <span class="text-muted">Ankush Bihal DWES 16/02/2021</span>
   </div>
 </footer>
 
